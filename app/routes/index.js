a719d0f23d77380cde24d1b71c33847a11603ac1
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-07-14 11:21:26 +0300

    I have added a model for the index router called question model.It contains hard coded data on questions,author,topic and an image relating to the question.I have then added a model in the index.js to return the object containing data with the question.On the index template i have displayed the information by looping over the model and displaying the name, author,question and image

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
-
+var questions = [{
+  id: 1,
+  author: "Veruca Salt",
+  topic: "Estate",
+  question:"what is life",
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+}, {
+  id: 2,
+  author: "collins",
+  topic: "tech",
+  question:"what is html",
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+}, {
+  id: 1,
+  author: "leah",
+  topic: "business",
+  question:"what is VAT",
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+}];
 export default Ember.Route.extend({
+  model(){
+    return questions;
+  },
 });
